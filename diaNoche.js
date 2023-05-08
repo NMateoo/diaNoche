8ec60640104bc9d58125fb97a7f2caab3febb35b
Nicolas Mateo <55945912+NMateoo@users.noreply.github.com>
2023-05-08 19:23:58 +0200

    1

--- diaNoche.js
+++ diaNoche.js
@@ -2,7 +2,39 @@
 
 var modo=leerModoActual();
 
+if (modo=="dia") {
+    ponerModoDia();
+}else {
+    ponerModoNoche();
+}
+
 function leerModoActual() {
     let modo=window.localStorage.getItem("modoGuardado");
 
+    if (!modo) {
+        modo="dia";
+    }
+    return modo;
+}
+
+function ponerModoDia() {
+    let todo=document.getElementById("todo");
+    todo.style.color="black"
+    todo.style.backgroundColor="white"
+    window.localStorage.setItem("modoGuardado","dia")
+    let iconoDia=document.getElementById("dia");
+    iconoDia.style.display="none";
+    let iconoNoche=document.getElementById("noche");
+    iconoNoche.style.display="block";
+}
+
+function ponerModoNoche() {
+    let todo=document.getElementById("todo");
+    todo.style.color="white"
+    todo.style.backgroundColor="black"
+    window.localStorage.setItem("modoGuardado","noche")
+    let iconoDia=document.getElementById("dia");
+    iconoDia.style.display="block";
+    let iconoNoche=document.getElementById("noche");
+    iconoNoche.style.display="none";
 }
